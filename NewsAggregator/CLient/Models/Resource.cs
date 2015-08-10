397893cfb0ff5fe09ab23be393aabff610c7cce0
Lateralus- <elena.skorchenko.y@gmail.com>
2015-08-10 21:56:11 +0300

    Architecture changes

--- NewsAggregator/CLient/Models/Resource.cs
+++ NewsAggregator/CLient/Models/Resource.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace NewsAggregator.Models 
+namespace NewsAggregator.Client.Models 
 {
 	public class Resource
 	{
